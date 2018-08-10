e44d516705566144378f90b08f250ab2f25eeef2
Rajendra Adhikari <rpadhikari@gmail.com>
2018-08-10 02:32:45 +0000

    Create bessel.f90

---
+++ Circular/bessel.f90
@@ -0,0 +1,10 @@
+real(8) function bessel(n, x)
+  implicit none
+  integer(2) i, n, m
+  real(8) sum, x, fact
+  sum=0.0d0
+  do m=0, 15
+    sum=sum+(-1)**m*(x/2.0d0)**(2*m+n)/(fact(m)*fact(m+n))
+  end do
+  bessel=sum
+end function bessel
